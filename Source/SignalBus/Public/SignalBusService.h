ee6a72a4f3fb4fae1ff1e5f5ec3f22d7b1db34f1
NorthCopibara <ElshinAleksandr@mail.ru>
2022-11-28 11:27:03 +0300

    Check func

--- Source/SignalBus/Public/SignalBusService.h
+++ Source/SignalBus/Public/SignalBusService.h
@@ -6,11 +6,14 @@
 #include "UObject/Object.h"
 #include "SignalBusService.generated.h"
 
+DEFINE_LOG_CATEGORY_STATIC(LogSignalBus, All, All);
+
 USTRUCT(BlueprintType)
 struct FSignalContext
 {
 	GENERATED_BODY()
 
+	FName FuncName;
 	TWeakObjectPtr<UObject> Context;
 	TWeakObjectPtr<UFunction> Func;
 };
@@ -27,43 +30,51 @@ UCLASS()
 class SIGNALBUS_API USignalBusService : public UObject
 {
 	GENERATED_BODY()
-	
+
 public:
 	template <typename T, typename UserClass>
 	void Bind(UserClass* Context, FName FuncName)
 	{
 		FSignal Signal;
-		
-		if(Signals.Contains(T::StaticStruct()->GetName()))
+
+		if (Signals.Contains(T::StaticStruct()->GetName()))
 		{
-			UE_LOG(LogTemp, Warning, TEXT("Test add signal"))
+			for (auto SignalContext : Signals[T::StaticStruct()->GetName()].SignalsContexts)
+			{
+				if(SignalContext.FuncName == FuncName && Context == SignalContext.Context.Get())
+				{
+					UE_LOG(LogSignalBus, Warning, TEXT("Function %s was binding"), *FuncName.ToString())
+					return;
+				}
+			}
+			
 			Signal = Signals[T::StaticStruct()->GetName()];
 		}
 
 		FSignalContext SignalContext;
 		SignalContext.Context = Cast<UObject>(Context);
-		SignalContext.Func = SignalContext.Context->FindFunction(FuncName);
+		SignalContext.Func = SignalContext.Context->FindFunctionChecked(FuncName);
+		SignalContext.FuncName = FuncName;
 		Signal.SignalsContexts.Add(SignalContext);
 
-		UE_LOG(LogTemp, Warning, TEXT("Test_func: %s"), *FuncName.ToString())
-		UE_LOG(LogTemp, Warning, TEXT("Test_bind: %s"), *T::StaticStruct()->GetName())
-		
 		Signals.Emplace(T::StaticStruct()->GetName(), Signal);
 	}
 
 	template <typename T>
 	void Send(T SignalData)
 	{
-		if(Signals.Contains(T::StaticStruct()->GetName()))
+		if (!Signals.Contains(T::StaticStruct()->GetName()))
 		{
-			void* OutData = static_cast<void*>(&SignalData);
-			
-			const auto Signal = Signals[T::StaticStruct()->GetName()];
-			for (auto SignalContext : Signal.SignalsContexts)
-			{
-				//TODO: check valid signal
-				SignalContext.Context->ProcessEvent(SignalContext.Func.Get(), OutData);
-			}
+			UE_LOG(LogSignalBus, Warning, TEXT("Bindings not found"))
+			return;
+		}
+
+		void* OutData = static_cast<void*>(&SignalData);
+
+		const auto Signal = Signals[T::StaticStruct()->GetName()];
+		for (auto SignalContext : Signal.SignalsContexts)
+		{
+			SignalContext.Context->ProcessEvent(SignalContext.Func.Get(), OutData);
 		}
 	}
 
